d1f3a1adfabb351ab6c4502afac4871d77bfae2e
ozzy <ozzy@cydeo.com>
2022-09-29 15:30:59 -0400

    Ticketing Part-1

--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -4,10 +4,6 @@ import com.cydeo.dto.UserDTO;
 
 import java.util.List;
 
-public interface UserService {
+public interface UserService extends CrudService<UserDTO,String> {
 
-    UserDTO save(UserDTO user);
-    UserDTO findById(String username);
-    List<UserDTO> findAll();
-    void deleteById(String username);
 }
