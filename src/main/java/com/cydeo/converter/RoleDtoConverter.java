8033b7572f68ff1c06269f4a16e9a147ee86fdbd
ozzy <ozzy@cydeo.com>
2022-09-30 16:33:17 -0400

    Ticketing Part-2

---
+++ src/main/java/com/cydeo/converter/RoleDtoConverter.java
@@ -0,0 +1,14 @@
+package com.cydeo.converter;
+
+import com.cydeo.dto.RoleDTO;
+import org.springframework.core.convert.converter.Converter;
+
+public class RoleDtoConverter implements Converter<String, RoleDTO> {  //"2"
+
+
+
+    @Override
+    public RoleDTO convert(String source) {
+        return null;
+    }
+}
