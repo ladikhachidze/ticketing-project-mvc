375555d3b093b08e05e1d96f2c25c32970aed218
ozzy <ozzy@cydeo.com>
2022-10-02 11:34:48 -0400

    Ticketing Part-3

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -5,10 +5,7 @@ import com.cydeo.service.RoleService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
@@ -44,6 +41,22 @@ public class UserController {
 
         return "redirect:/user/create";
 
+    }
+
+    @GetMapping("/update/{username}")
+    public String editUser(@PathVariable("username") String username, Model model){
+
+
+        //user object ${user}
+        model.addAttribute("user",userService.findById(username));
+
+
+        //roles ${roles}
+
+
+        //users ${users}
+
+        return "/user/update";
     }
 
 
